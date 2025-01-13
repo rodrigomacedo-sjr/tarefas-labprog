57f2aba4837ae5b5050b612b340ff657ba5fcfee
Rodrigo M Silva <rodrigomacedo.sjr@gmail.com>
2025-01-13 12:32:51 -0300

    primeiro commit!
    Projeto criado com start.spring.io

---
+++ src/main/java/com/gestaotarefas/gestaotarefas/GestaotarefasApplication.java
@@ -0,0 +1,13 @@
+package com.gestaotarefas.gestaotarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaotarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaotarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaotarefas/gestaotarefas/GestaotarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaotarefas.gestaotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaotarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
