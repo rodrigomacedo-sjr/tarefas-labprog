ad88307b143a1528672814c6c9150cc67f3889be
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-01-17 15:21:12 -0300

    feat: listar tarefas

--- src/main/java/com/uel/tarefas/TarefaController.java
+++ src/main/java/com/uel/tarefas/TarefaController.java
@@ -2,6 +2,7 @@ package com.uel.tarefas;
  
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +15,16 @@ public class TarefaController {
    com.uel.tarefas.TarefaRepository tarefaRepository;
  
    @GetMapping("/nova-tarefa")
-   public String mostrarFormNovaTarefa(Tarefa tarefa){
+   public String mostrarFormNovaTarefa(Tarefa tarefa) {
        return "/nova-tarefa";
    }
+
+   @GetMapping(value={"/index", "/"})
+   public String mostrarListaTarefas(Model model) {
+      model.addAttribute("tarefas", tarefaRepository.findAll());
+      return "index";
+   }
+
  
    @PostMapping("/adicionar-tarefa")
    public String adicionarTarefa(@Valid Tarefa tarefa, 
