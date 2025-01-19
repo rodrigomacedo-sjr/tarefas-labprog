a800a63265fdc4cfe47e5f3829c513727b7482f9
rodrigomacedo-sjr <rodrigomacedo.sjr@gmail.com>
2025-01-19 12:56:49 -0300

    feat: atualizar tarefas

--- src/main/java/com/uel/tarefas/TarefaController.java
+++ src/main/java/com/uel/tarefas/TarefaController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import jakarta.validation.Valid;
  
@@ -27,13 +28,31 @@ public class TarefaController {
 
  
    @PostMapping("/adicionar-tarefa")
-   public String adicionarTarefa(@Valid Tarefa tarefa, 
-                                         BindingResult result) {
+   public String adicionarTarefa(@Valid Tarefa tarefa, BindingResult result) {
        if (result.hasErrors()) {
            return "/nova-tarefa";
        }
- 
        tarefaRepository.save(tarefa);
        return "redirect:/nova-tarefa";
+   }
+
+   @GetMapping("/editar/{id}")
+   public String mostrarFormAtualizar(@PathVariable("id") int id, Model model) {
+       Tarefa tarefa = tarefaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(
+                   "O id da tarefa é inválido: " + id));
+
+       model.addAttribute("tarefa", tarefa);
+       return "atualizar-tarefa";
+   }
+
+   @PostMapping("/atualizar/{id}")
+   public String atualizarTarefa(@PathVariable("id") int id, @Valid Tarefa tarefa, BindingResult result, Model model) {
+       if (result.hasErrors()) {
+           tarefa.setId(id);
+           return "atualizar-tarefa";
+       }
+
+       tarefaRepository.save(tarefa);
+       return "redirect:/index";
    }
 }
